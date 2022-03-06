89736927e74fa154b458bc68bf6ccb013c22d3eb
Ola Næss Kaldestad <mail@okal.no>
2022-03-06 15:41:11 +0100

    more refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,192 +1,14 @@
-pub mod report;
+pub mod args;
+pub mod lib;
 pub mod config;
-pub mod color;
-pub mod none;
+pub mod report;
 
-use clap::{ self, Parser, Subcommand };
+use clap::Parser;
 use hidapi::HidApi;
-use none::None;
-use color::Color;
 
-#[derive(Parser)]
-#[clap(
-    // From Cargo.toml
-    author, version, about,
-
-    // Don't need a subcommand when we have flags
-    disable_help_subcommand = true,
-
-    // Same reported version for subcommands
-    propagate_version = true,
-
-    // Prevent automatic uppercase short version
-    mut_arg("version", |x| { x.short('v') }),
-)]
-struct Args {
-    #[clap(subcommand)]
-    kind: Kind,
-}
-
-#[derive(Subcommand)]
-enum Kind {
-    /// Retrieve information about the device
-    #[clap(subcommand)]
-    Report(Report),
-
-    /// Change the device's various settings
-    #[clap(subcommand)]
-    Config(Config),
-
-    #[clap(hide = true)]
-    Hex {
-        #[clap(
-            min_values = 1, max_values = 3,
-            parse(try_from_str = color::parse_hex)
-        )]
-        colors: Vec<Color>
-    },
-}
-
-#[derive(Subcommand)]
-enum Report {
-    /// Battery percentage (if available)
-    Battery,
-
-    /// Device firmware version
-    Firmware,
-}
-
-#[derive(Subcommand)]
-enum Config {
-    /// Active profile by id
-    Profile {
-        #[clap(possible_values(["1", "2", "3"]))]
-        id: u8
-    },
-
-    /// LED Effect
-    LEDEffect {
-        // Profile id (1-3)
-        #[clap(
-            short, long,
-            help = "[default: 1]",
-            possible_values(["1", "2", "3"])
-        )]
-        profile: Option<u8>,
-
-        #[clap(subcommand)]
-        effect: Effect
-    },
-
-    /// LED brightness value[s] (0-255)
-    LEDBrightness {
-        wired: u8,
-
-        #[clap(help = "[default: <WIRED>]")]
-        wireless: Option<u8>,
-    },
-
-    /// Sleep delay in minutes [and seconds]
-    Sleep {
-        minutes: u8,
-
-        #[clap(help = "[default: 0]")]
-        seconds: Option<u8>,
-    },
-
-    /// (not implemented)
-    DPIStages,
-
-    /// (not implemented)
-    DPIStage,
-
-    /// (not implemented)
-    DPIColor,
-
-    /// (not implemented)
-    Calibration,
-
-    /// (not implemented)
-    PollingRate,
-
-    /// (not implemented)
-    Debounce,
-
-    /// (not implemented)
-    Macro,
-
-    /// (not implemented)
-    Key,
-}
-
-#[derive(Subcommand)]
-pub enum Effect {
-    /// Name says it all
-    Glorious {
-        /// Effect rate, 0-100
-        #[clap(short, long)]
-        rate: Option <u8>,
-    },
-
-    /// Cycle through all colors
-    Cycle {
-        /// Effect rate, 0-100
-        #[clap(short, long)]
-        rate: Option<u8>,
-    },
-
-    /// Pulse on/off through given colors
-    Pulse {
-        /// Effect rate, 0-100
-        #[clap(short, long)]
-        rate: Option<u8>,
-
-        /// From 2 to 6 colors in hex format
-        #[clap(
-            min_values = 1, max_values = 6,
-            parse(try_from_str = color::parse_hex)
-        )]
-        colors: Vec<Color>
-    },
-
-    /// Solid color
-    Solid {
-        /// Color in hex format
-        #[clap(parse(try_from_str = color::parse_hex))]
-        color: Color
-    },
-
-    /// Glorious, but colors don't "move"
-    Tail {
-        /// Effect rate, 0-100
-        #[clap(short, long)]
-        rate: Option<u8>,
-    },
-
-    /// Strobe-like effect
-    Rave {
-        /// Effect rate, 0-100
-        #[clap(short, long)]
-        rate: Option<u8>,
-
-        /// 1 or 2 colors in hex format
-        #[clap(
-            min_values = 1, max_values = 2,
-            parse(try_from_str = color::parse_hex)
-        )]
-        colors: Vec<Color>
-    },
-
-    /// Glorious, but more circus
-    Wave {
-        /// Effect rate, 0-100
-        #[clap(short, long)]
-        rate: Option<u8>,
-    },
-
-    /// No effect, LED off
-    Off, 
-}
+use lib::color;
+use lib::none::None;
+use args::{ Args, Kind, Report, Config };
 
 fn main() {
     // Parse the command line arguments
@@ -212,7 +34,7 @@ fn main() {
     let wired = device_info.product_id() == 0x2011;
 
     // Connect to the device
-    let device = device_info.open_device(&hid_api).unwrap();
+    let device = hid_api.open_path(device_info.path()).unwrap();
 
     // Act upon command line arguments
     match args.kind {
